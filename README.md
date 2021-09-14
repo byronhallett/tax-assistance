1e3664cc4327183071785ffbc5f5163f50f2f2ba
Byron Hallett <byron.james.h@gmail.com>
2021-09-14 15:29:32 +1000

    mac cleanup

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Tax Assistance
 
 A serverless function for zeit-now to fulfill tax assistance action on google
+
+based on tax rates for 2019-2020:
+| income             | percent_tax |
+| ------------------ | ----------- |
+| $0 – $18,200       | 0%          |
+| $18,201 – $37,000  | 19%         |
+| $37,001 – $90,000  | 32.5%       |
+| $90,001 – $180,000 | 37%         |
+| $180,000 and over  | 45%         |
--- api/main.go
+++ api/main.go
@@ -1,116 +1,39 @@
+// Package handler contains an HTTP Cloud Function.
 package handler
 
 import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
-type queryResult struct {
-	QueryText  string
-	Action     string
-	Parameters struct {
-		Number       int
-		UnitCurrency string
-	}
+type data struct {
+	QueryResult struct {
+		Parameters struct {
+			Number       int64 `json:"number"`
+			UnitCurrency struct {
+				Amount int64 `json:"amount"`
+			} `json:"unit-currency"`
+		} `json:"parameters"`
+	} `json:"queryResult"`
 }
 
-type dialogRequest struct {
-	ResponseID  string
-	QueryResult queryResult
-}
-
-// Handler is my serverless function
+// Handler does thing
 func Handler(w http.ResponseWriter, r *http.Request) {
-	var info dialogRequest
-	err := json.NewDecoder(r.Body).Decode(&info)
-	if err != nil {
-		fmt.Println(err)
+	var d data
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, err)
+		return
 	}
-	fmt.Println("Test print")
-	Spew.dump(info.QueryResult.QueryText)
-	fmt.Fprintf(w, info.QueryResult.QueryText)
+	var value int64 = 0
+	if d.QueryResult.Parameters.Number != "" {
+		value, _ = strconv.ParseInt(d.QueryResult.Parameters.Number, 10, 64)
+	}
+	if d.QueryResult.Parameters.UnitCurrency.Amount != "" {
+		value, _ = strconv.ParseInt(d.QueryResult.Parameters.UnitCurrency.Amount, 10, 64)
+	}
+	// TODO: choose between the number and currency amount fields
+	// TODO: calculate withholdable amount
+	fmt.Fprintf(w, "Withhold $%d. Any further calculations?", value)
 }
-
-//     "allRequiredParamsPresent": true,
-//     "fulfillmentText": "Withhold $100. Any other income to calculate?",
-//     "fulfillmentMessages": [
-//       {
-//         "text": {
-//           "text": [
-//             "Withhold $100. Any other income to calculate?"
-//           ]
-//         }
-//       }
-//     ],
-
-//   },
-//   "originalDetectIntentRequest": {
-//     "source": "google",
-//     "version": "2",
-//     "payload": {
-//       "user": {
-//         "locale": "en-US",
-//         "lastSeen": "2020-01-25T18:08:51Z",
-//         "userVerificationStatus": "VERIFIED"
-//       },
-//       "conversation": {
-//         "conversationId": "ABwppHH6ZiJlZEg5pdOTFKazUSsuZu2BOqVR3t3tATRqKdZGmWkDMZK25IUZcCvDDxc9VEzca5T-Kr-T-W8",
-//         "type": "ACTIVE",
-//         "conversationToken": "[]"
-//       },
-//       "inputs": [
-//         {
-//           "intent": "actions.intent.TEXT",
-//           "rawInputs": [
-//             {
-//               "inputType": "VOICE",
-//               "query": "123"
-//             }
-//           ],
-//           "arguments": [
-//             {
-//               "name": "text",
-//               "rawText": "123",
-//               "textValue": "123"
-//             }
-//           ]
-//         }
-//       ],
-//       "surface": {
-//         "capabilities": [
-//           {
-//             "name": "actions.capability.ACCOUNT_LINKING"
-//           },
-//           {
-//             "name": "actions.capability.MEDIA_RESPONSE_AUDIO"
-//           },
-//           {
-//             "name": "actions.capability.AUDIO_OUTPUT"
-//           },
-//           {
-//             "name": "actions.capability.SCREEN_OUTPUT"
-//           }
-//         ]
-//       },
-//       "isInSandbox": true,
-//       "availableSurfaces": [
-//         {
-//           "capabilities": [
-//             {
-//               "name": "actions.capability.AUDIO_OUTPUT"
-//             },
-//             {
-//               "name": "actions.capability.SCREEN_OUTPUT"
-//             },
-//             {
-//               "name": "actions.capability.WEB_BROWSER"
-//             }
-//           ]
-//         }
-//       ],
-//       "requestType": "SIMULATOR"
-//     }
-//   },
-//   "session": "projects/tax-time-c294a/agent/sessions/ABwppHH6ZiJlZEg5pdOTFKazUSsuZu2BOqVR3t3tATRqKdZGmWkDMZK25IUZcCvDDxc9VEzca5T-Kr-T-W8"
-// }
